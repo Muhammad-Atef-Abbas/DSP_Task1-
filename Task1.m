68f3773e37b192002ffa335f281bb511ec352e8a
Muhammad-Atef-Abbas <93487356+Muhammad-Atef-Abbas@users.noreply.github.com>
2021-10-31 19:49:02 +0200

    Add files via upload
    DSP Task 1

---
+++ Task1.m
@@ -0,0 +1,34 @@
+%%Moahmed Atef Abbas (B4)
+%% program #1
+clear
+clc
+A=[-7 5 -9;2 -1 2;1 -1 2];
+B=[16 3 2 13;5 10 11 8;9 6 7 12;4 15 14 1];
+C=[4 2 -3; 7 -7 9;3 -5 6];
+D=[6 3 2;2 12 -7;-1 6 2;-5 15 11];
+3*A-5*C
+C*A
+C*D'
+7*A+2*B %%because the dimensions not the same
+%% program #2
+clc
+zeros(3)
+zeros(3,2)
+ones(3)
+ones(3,2)
+size(D)
+zeros(size(D))
+diag([1 2 3 4])
+eye(3)
+%% program #3
+[A,B]%%because the dimensions not the same
+[A;B]%%because the dimensions not the same
+%% program #4
+clear
+clc
+a=diag([5 5 5 5 5 5 5]);
+a(:,8)=[5 5 5 5 5 5 5]
+
+%% program #5
+a(:,end)
+a(1,:)
